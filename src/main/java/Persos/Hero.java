be7b5a7dfb10f587ac784e658eee226eca00dd9e
RSymbiose <romain.jacovetti@etudiant.univ-rennes1.fr>
2024-11-19 11:43:25 +0100

    Hero

--- src/main/java/Persos/Hero.java
+++ src/main/java/Persos/Hero.java
@@ -1,4 +1,20 @@
 package Persos;
 
-public class Hero extends Personnages{
+public abstract class Hero extends Personnages {
+    private boolean capaciteSpecialeUtilisee;
+
+    public Hero(String nom, int pointsDeVie, int attaque, int defense) {
+        super(nom, pointsDeVie, attaque, defense);
+        this.capaciteSpecialeUtilisee = false;
+    }
+
+    public abstract void utiliserCapaciteSpeciale(Personnages cible);
+
+    public boolean isCapaciteSpecialeUtilisee() {
+        return capaciteSpecialeUtilisee;
+    }
+
+    public void setCapaciteSpecialeUtilisee(boolean capaciteSpecialeUtilisee) {
+        this.capaciteSpecialeUtilisee = capaciteSpecialeUtilisee;
+    }
 }

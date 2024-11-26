e664f4b7ce1fcf3980a9b3aacc8c8c12e366e643
schweyer-9 <99031881+schweyer-9@users.noreply.github.com>
2024-11-26 11:43:23 +0100

    Modif Carte et évoluation niveau Jungle (add difficulté - spawnplace)

--- src/main/java/Niveaux/Carte.java
+++ src/main/java/Niveaux/Carte.java
@@ -3,16 +3,22 @@ package Niveaux;
 import java.util.ArrayList;
 
 public class Carte {
-    private ArrayList<Object> Map;
     private double difficulty;
 
     public Carte(){
-        this.Map=null;
         this.difficulty=0;
     }
 
     public Carte(ArrayList<Object> map, double difficulty){
         this.Map=map;
         this.difficulty=difficulty;
+    }
+
+    public double getDifficulty() {
+        return difficulty;
+    }
+
+    public void setDifficulty(double difficulty) {
+        this.difficulty = difficulty;
     }
 }
--- src/main/java/Niveaux/Jungle.java
+++ src/main/java/Niveaux/Jungle.java
@@ -1,5 +1,24 @@
 package Niveaux;
 
-public class Jungle extends Carte{
+import java.util.ArrayList;
 
+public class Jungle extends Carte{
+    private ArrayList<Object> Map;
+
+    public Jungle(){
+        super();
+        int longueur = 9;
+        this.Map=new ArrayList<Object>(longueur);
+    }
+
+    public void Difficulte(int diff){
+        super.setDifficulty(diff);
+        ArrayList<Integer> spawnPlaces = new ArrayList<Integer>(diff);
+        for(int i=0; i<diff;i++){
+            int indice = (int)(Math.random()*diff+1);
+            if (!spawnPlaces.contains(indice)){
+                spawnPlaces.add(indice);}
+        }
+        // spawnability :
+    }
 }

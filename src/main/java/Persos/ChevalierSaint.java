137c41e1953ffc7d36b6427df1972ef7e0bb4b7f
RSymbiose <118276398+RSymbiose@users.noreply.github.com>
2024-12-03 10:34:11 +0100

    Update ChevalierSaint.java

--- src/main/java/Persos/ChevalierSaint.java
+++ src/main/java/Persos/ChevalierSaint.java
@@ -2,9 +2,41 @@ package Persos;
 
 public class ChevalierSaint extends Hero {
     //Peut purifier un ennemi, celui-ci inflige 50% de dégâts en moins
-
-    public ChevalierSaint(String nom){
+    private Personnages personnagePurifie;
+    private boolean purificationActive;
+    
+    public ChevalierSaint(String nom) {
         super(nom, 100, 30, 20);
+        this.purificationActive = false;
+        this.personnagePurifie = null;
     }
-
+    
+    @Override
+    public void utiliserCapaciteSpeciale(Personnages cible) {
+        if (!isCapaciteSpecialeUtilisee()) {
+            this.personnagePurifie = cible;
+            this.purificationActive = true;
+            setCapaciteSpecialeUtilisee(true);
+            System.out.println(getNom() + " purifie " + cible.getNom() + " ! Ses dégâts sont réduits de moitié !");
+            
+            // Réduit l'attaque de la cible de moitié
+            int nouvelleAttaque = cible.getAttaque() / 2;
+            cible.setAttaque(nouvelleAttaque);
+        } else {
+            System.out.println("La capacité spéciale a déjà été utilisée !");
+        }
+    }
+    
+    public boolean estPurifie(Personnages personnage) {
+        return purificationActive && personnagePurifie == personnage;
+    }
+    
+    public void finPurification() {
+        if (personnagePurifie != null && purificationActive) {
+            // Restaure l'attaque originale
+            personnagePurifie.setAttaque(personnagePurifie.getAttaque() * 2);
+            this.purificationActive = false;
+            this.personnagePurifie = null;
+        }
+    }
 }

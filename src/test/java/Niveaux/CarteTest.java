a260b2085b4b37b5c22612965129cc2e6710fb84
schweyer-9 <99031881+schweyer-9@users.noreply.github.com>
2024-12-08 18:19:32 +0100

    Création Tests pour carte

--- src/test/java/Niveaux/CarteTest.java
+++ src/test/java/Niveaux/CarteTest.java
@@ -1,4 +1,31 @@
 package Niveaux;
 
-public class CarteTest {
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+class CarteTest {
+
+    @Test
+    void getDifficulty() {
+    }
+
+    @Test
+    void setDifficulty() {
+    }
+
+    @Test
+    void getMap() {
+    }
+
+    @Test
+    void setMap() {
+    }
+
+    @Test
+    void getNom() {
+    }
+
+    @Test
+    void setNom() {
+    }
 }

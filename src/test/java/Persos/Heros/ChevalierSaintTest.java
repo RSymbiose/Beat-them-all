e0aca4821c80a5567260a876320dff6eb68b9257
schweyer-9 <99031881+schweyer-9@users.noreply.github.com>
2024-12-08 21:56:13 +0100

    Tests pour toutes les classes Ennemies

--- src/test/java/Persos/Heros/ChevalierSaintTest.java
+++ src/test/java/Persos/Heros/ChevalierSaintTest.java
@@ -1,20 +1,21 @@
 package Persos.Heros;
 
+import Persos.Brigand;
+import Persos.ChevalierSaint;
 import org.junit.jupiter.api.Test;
 
 import static org.junit.jupiter.api.Assertions.*;
 
 class ChevalierSaintTest {
-
     @Test
-    void utiliserCapaciteSpeciale() {
-    }
-
-    @Test
-    void estPurifie() {
-    }
-
-    @Test
-    void finPurification() {
+    void Attaquer() {
+        //Note: le test de one shot d'un ennemi par le héro est obligatoire, mais la quantité de dégats infligé est fait de manière random dans notre programme
+        // -> Test effectué en deux parties: le héro inflige des dégats, puis l'ennemi se prend assez de dégats pour être oneshot.
+        ChevalierSaint CS1 = new ChevalierSaint("Henri");
+        Brigand B1 = new Brigand("Dalton");
+        CS1.attaquer(B1);
+        int degats = B1.getPointsDeVie();
+        B1.prendreDegats(degats);
+        assertEquals(B1.getPointsDeVie(), 0);
     }
 }

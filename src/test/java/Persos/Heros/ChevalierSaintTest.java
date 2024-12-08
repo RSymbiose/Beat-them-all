0b2c587102995d84c96a12adde072b74c3a05395
schweyer-9 <99031881+schweyer-9@users.noreply.github.com>
2024-12-08 21:57:29 +0100

    Test obligatoire pour le Heros (dans la classe chevalier saint) (ps: pas possible de faire des tests sur des fonctions qui ne retournent rien, donc pas d'autres tests dans les héros)

--- src/test/java/Persos/Heros/ChevalierSaintTest.java
+++ src/test/java/Persos/Heros/ChevalierSaintTest.java
@@ -7,6 +7,7 @@ import org.junit.jupiter.api.Test;
 import static org.junit.jupiter.api.Assertions.*;
 
 class ChevalierSaintTest {
+
     @Test
     void Attaquer() {
         //Note: le test de one shot d'un ennemi par le héro est obligatoire, mais la quantité de dégats infligé est fait de manière random dans notre programme

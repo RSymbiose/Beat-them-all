10ffc7dbd3a212738179cae89febf4486d9a4153
schweyer-9 <99031881+schweyer-9@users.noreply.github.com>
2024-12-08 22:42:34 +0100

    Test obligatoire pour le Heros (dans la classe chevalier saint) (ps: pas possible de faire des tests sur des fonctions qui ne retournent rien, donc pas d'autres tests dans les héros)

---
+++ src/main/java/Persos/HeroInterface.java
@@ -0,0 +1,8 @@
+package Persos;
+
+public interface HeroInterface {
+    boolean capaciteSpecialeUtilisee = false;
+    public abstract void utiliserCapaciteSpeciale(Personnages cible);
+    public boolean isCapaciteSpecialeUtilisee();
+    public void setCapaciteSpecialeUtilisee(boolean capaciteSpecialeUtilisee);
+}

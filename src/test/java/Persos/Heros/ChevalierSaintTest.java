82782ff6f967cab73e018f938114933ca4e001b7
schweyer-9 <99031881+schweyer-9@users.noreply.github.com>
2024-12-09 14:37:26 +0100

    Continuation test hero obligatoire

--- src/test/java/Persos/Heros/ChevalierSaintTest.java
+++ src/test/java/Persos/Heros/ChevalierSaintTest.java
@@ -1,8 +1,11 @@
 package Persos.Heros;
 
+import Niveaux.Jungle;
 import Persos.Brigand;
 import Persos.ChevalierSaint;
 import org.junit.jupiter.api.Test;
+
+import java.util.ArrayList;
 
 import static org.junit.jupiter.api.Assertions.*;
 
@@ -10,10 +13,13 @@ class ChevalierSaintTest {
 
     @Test
     void Attaquer() {
-        //Note: le test de one shot d'un ennemi par le héro est obligatoire, mais la quantité de dégats infligé est fait de manière random dans notre programme
-        // -> Test effectué en deux parties: le héro inflige des dégats, puis l'ennemi se prend assez de dégats pour être oneshot.
         ChevalierSaint CS1 = new ChevalierSaint("Henri");
         Brigand B1 = new Brigand("Dalton");
+        ArrayList<Object> ennemi = new ArrayList<Object>(B1);
+        Jungle J1 = new Jungle();
+        J1.setMap(ennemi);
+        //Note: le test de one shot d'un ennemi par le héro est obligatoire, mais la quantité de dégats infligé est fait de manière random dans notre programme
+        // -> Test effectué en deux parties: le héro inflige des dégats, puis l'ennemi se prend assez de dégats pour être oneshot
         CS1.attaquer(B1);
         int degats = B1.getPointsDeVie();
         B1.prendreDegats(degats);
